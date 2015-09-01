23d7898debc5b1a20375cf2ad6b70de0df741dd8
sonitgregorio <sonitgregorio@gmail.com>
2015-09-01 17:32:30 +0800

    Edited view

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -1,6 +1,5 @@
   <div id="page-content-wrapper">
-    <a href="#menu-toggle" id="menu-toggle" class="btn btn-info" style="margin-left:-30px;margin-top:-50px"></a>
-
+    <a href="#menu-toggle" id="menu-toggle" class="btn btn-info" style="margin-left:-30px;margin-top:-30px; height:40px;background: #B22222;">Menu</a>
       <div class="container-fluid">
           <div class="row">
               <div class="col-lg-12">
@@ -9,7 +8,11 @@
                       <h1 class="panel-title" style="color:white">Hello World</h1>
                     </div>
                     <div class="panel-body">
-
+                        <form class="form-horizontal" action="index.html" method="post">
+                          <div class="form-group">
+                            <label class="col-sm-3"></label>
+                          </div>
+                        </form>
                     </div>
                   </div>
               </div>
