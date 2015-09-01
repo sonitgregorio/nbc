b6c19c5f36303121d4f24e18fa6a8a52cc03b356
sonitgregorio <sonitgregorio@gmail.com>
2015-09-01 16:44:36 +0800

    Edited toggle menu

--- application/views/page/home.php
+++ application/views/page/home.php
@@ -1,8 +1,10 @@
   <div id="page-content-wrapper">
+    <a href="#menu-toggle" id="menu-toggle" class="btn btn-info" style="margin-left:-30px;margin-top:-50px"></a>
+
       <div class="container-fluid">
           <div class="row">
               <div class="col-lg-12">
-                  <div class="panel panel-default">
+                    <div class="panel panel-default">
                     <div class="panel-heading" style="background: rgb(157, 90, 71)" >
                       <h1 class="panel-title" style="color:white">Hello World</h1>
                     </div>
@@ -10,7 +12,6 @@
 
                     </div>
                   </div>
-                  <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
               </div>
           </div>
       </div>
