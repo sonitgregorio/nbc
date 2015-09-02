0a3a240a783328f503bd9507b1c93fccf1dfb5b9
sonitgregorio <sonitgregorio@gmail.com>
2015-09-02 10:09:22 +0800

    Added icon

--- application/views/include/nav.php
+++ application/views/include/nav.php
@@ -1,35 +1,35 @@
 <div id="wrapper">
     <!-- Sidebar -->
-    <div id="sidebar-wrapper" style="background: #B22222;box-shadow: 0px 0px 10px black">
+    <div id="sidebar-wrapper" style="background: rgb(157, 90, 71);box-shadow: 0px 0px 10px black">
         <ul class="sidebar-nav">
-            <li class="sidebar-brand">
-                <a href="#">
-                    Start Bootstrap
+            <li class="sidebar-brand" style="padding:0;margin:0 auto">
+                <a href="#" style="color:white">
+                  <img src="../assets/images/EVSU.jpg" alt="x" width="40" style="border-radius:20px"/> EVSU
                 </a>
             </li>
             <li>
-                <a href="#">Home</a>
+                <a href="#" style="color:white">Home</a>
             </li>
             <li>
-                <a href="/user_registration">User Registration</a>
+                <a href="/user_registration" style="color:white">User Registration</a>
             </li>
             <li>
-                <a href="/faculty_registration">Faculty Registration</a>
+                <a href="/faculty_registration" style="color:white">Faculty Registration</a>
             </li>
             <li>
-                <a href="/add_criteria">ADD Common CCE</a>
+                <a href="/add_criteria" style="color:white">ADD Common CCE</a>
             </li>
             <li>
-                <a href="/add_school">Add School</a>
+                <a href="/add_school" style="color:white">Add School</a>
             </li>
             <li>
-                <a href="#">QCE</a>
+                <a href="#" style="color:white">QCE</a>
             </li>
             <li>
-                <a href="#">CCE</a>
+                <a href="#" style="color:white">CCE</a>
             </li>
             <li>
-                <a href="#">Reports</a>
+                <a href="#" style="color:white">Reports</a>
             </li>
         </ul>
     </div>
