60a71bc85893a95a372db279971c5f24e75b2c3d
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-08 17:07:07 +0800

    for sticky

--- application/views/index.php
+++ application/views/index.php
@@ -2,11 +2,11 @@
     <div class="panel panel-default" style="box-shadow: 0px 0px 20px rgb(49, 49, 49)">
         <div class="panel-heading" style="background: rgb(157, 90, 71)"><h3 class="panel-title" style="color:white; font-size:30px;">User Login</h3></div>
           <div class="panel-body">
-              <form class="form-horizontal" action="/login" method="post">
+              <form class="form-horizontal" action="/" method="post">
                   <div class="form-group">
                       <label for="" class="col-sm-3 control-label">Username</label>
                       <div class="col-sm-9">
-                        <input type="text" class="form-control" name="username" value="" placeholder="Username">
+                        <input type="text" class="form-control" name="username" value="<?php echo set_value('username') ?>" placeholder="Username">
                       </div>
                   </div>
                   <div class="form-group">
