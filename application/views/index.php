be1e8afa53b90055c6bbbc795e02517869d64cf6
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-12 23:29:59 +0800

    add icon

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,13 @@
 <div class="col-md-4 col-md-offset-4" style="margin-top: 18%">
     <div class="panel panel-default" style="box-shadow: 0px 0px 20px rgb(49, 49, 49)">
-        <div class="panel-heading" style="background: rgb(157, 90, 71)"><h3 class="panel-title" style="color:white; font-size:30px;">User Login</h3></div>
+        <div class="panel-heading" style="background: rgb(157, 90, 71)"><h3 class="panel-title" style="color:white; font-size:30px;"><span class="glyphicon glyphicon-user"></span>&nbsp;User Login</h3></div>
           <div class="panel-body">
               <form class="form-horizontal" action="/" method="post">
                   <?php echo $error ?>
                   <div class="form-group">
                       <label for="" class="col-sm-3 control-label">Username</label>
                       <div class="col-sm-9">
-                        <input type="text" class="form-control" name="username" value="<?php echo set_value('username') ?>" placeholder="Username">
+                        <input type="text" class="form-control" name="username" value="<?php echo set_value('username') ?>" placeholder="Username" autofocus>
                       </div>
                   </div>
                   <div class="form-group">
