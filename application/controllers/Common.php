d29d4d05e9b4f0a34381901a7aedaa3d631d02d2
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-12 23:12:19 +0800

    capitalize some input

--- application/controllers/Common.php
+++ application/controllers/Common.php
@@ -44,10 +44,10 @@ class Common extends CI_Controller
     function insert_faculty()
     {
         $data = array(
-                        'firstname'       => $this->input->post('firstname'),
-                        'middlename'      => $this->input->post('middlename'),
-                        'lastname'        => $this->input->post('lastname'),
-                        'address'         => $this->input->post('address'),
+                        'firstname'       => ucwords($this->input->post('firstname')),
+                        'middlename'      => ucwords($this->input->post('middlename')),
+                        'lastname'        => ucwords($this->input->post('lastname')),
+                        'address'         => ucwords($this->input->post('address')),
                         'emailaddress'    => $this->input->post('emailaddress'),
                         'contact'         => $this->input->post('contact'),
                         'position'        => $this->input->post('position'),
