711cb160c274b61aace7f7769bd674f8d8f69ecc
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-16 17:29:34 +0800

    javascript evaluation finish

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -5,9 +5,164 @@
   <script src="/assets/js/bootstrap.js"></script>
       <script>
       $(document).ready(function (){
+          var $dname11  = 0
+          var $dname21  = 0;
+          var $dname31  = 0;
+          var $dname41  = 0;
+          var $dname51  = 0;
+          var $total    = 0;
           $("#menu-toggle").click(function(e) {
               e.preventDefault();
               $("#wrapper").toggleClass("toggled");
+          });
+
+          $('.dname').click(function(){
+              $total_dname = $('#dname');
+              $dname1 = $('input[name=dname1]:checked');
+              if($dname1.length > 0)
+              {
+                  $dname11 = $dname1.val();
+              }
+              $dname2 = $('input[name=dname2]:checked');
+
+              if($dname2.length > 0)
+              {
+                  $dname21 = $dname2.val();
+              }
+              $dname3 = $('input[name=dname3]:checked');
+              $dname4 = $('input[name=dname4]:checked');
+              $dname5 = $('input[name=dname5]:checked');
+              if($dname3.length > 0)
+              {
+                  $dname31 = $dname3.val();
+              }
+              if($dname4.length > 0)
+              {
+                  $dname41 = $dname4.val();
+              }
+              if($dname5.length > 0)
+              {
+                  $dname51 = $dname5.val();
+              }
+              $total = Number($dname11) + Number($dname21) + Number($dname31) + Number($dname41) + Number($dname51);
+              $total_dname.html($total);
+          });
+
+          var $cname11  = 0
+          var $cname21  = 0;
+          var $cname31  = 0;
+          var $cname41  = 0;
+          var $cname51  = 0;
+          var $total    = 0;
+
+          $('.cname').click(function(){
+              $total_cname = $('#cname');
+              $dname1 = $('input[name=cname1]:checked');
+              if($dname1.length > 0)
+              {
+                  $cname11 = $dname1.val();
+              }
+              $dname2 = $('input[name=cname2]:checked');
+
+              if($dname2.length > 0)
+              {
+                  $cname21 = $dname2.val();
+              }
+              $dname3 = $('input[name=cname3]:checked');
+              $dname4 = $('input[name=cname4]:checked');
+              $dname5 = $('input[name=cname5]:checked');
+              if($dname3.length > 0)
+              {
+                  $cname31 = $dname3.val();
+              }
+              if($dname4.length > 0)
+              {
+                  $cname41 = $dname4.val();
+              }
+              if($dname5.length > 0)
+              {
+                  $cname51 = $dname5.val();
+              }
+              $total = Number($cname11) + Number($cname21) + Number($cname31) + Number($cname41) + Number($cname51);
+              $total_cname.html($total);
+          });
+
+          var $bname11  = 0
+          var $bname21  = 0;
+          var $bname31  = 0;
+          var $bname41  = 0;
+          var $bname51  = 0;
+          var $total    = 0;
+
+          $('.bname').click(function(){
+              $total_bname = $('#bname');
+              $dname1 = $('input[name=bname1]:checked');
+              if($dname1.length > 0)
+              {
+                  $bname11 = $dname1.val();
+              }
+              $dname2 = $('input[name=bname2]:checked');
+
+              if($dname2.length > 0)
+              {
+                  $bname21 = $dname2.val();
+              }
+              $dname3 = $('input[name=bname3]:checked');
+              $dname4 = $('input[name=bname4]:checked');
+              $dname5 = $('input[name=bname5]:checked');
+              if($dname3.length > 0)
+              {
+                  $bname31 = $dname3.val();
+              }
+              if($dname4.length > 0)
+              {
+                  $bname41 = $dname4.val();
+              }
+              if($dname5.length > 0)
+              {
+                  $bname51 = $dname5.val();
+              }
+              $total = Number($bname11) + Number($bname21) + Number($bname31) + Number($bname41) + Number($bname51);
+              $total_bname.html($total);
+          });
+
+          var $aname11  = 0
+          var $aname21  = 0;
+          var $aname31  = 0;
+          var $aname41  = 0;
+          var $aname51  = 0;
+          var $total    = 0;
+
+          $('.aname').click(function(){
+              $total_aname = $('#aname');
+              $dname1 = $('input[name=aname1]:checked');
+              if($dname1.length > 0)
+              {
+                  $aname11 = $dname1.val();
+              }
+              $dname2 = $('input[name=bname2]:checked');
+
+              if($dname2.length > 0)
+              {
+                  $aname21 = $dname2.val();
+              }
+              $dname3 = $('input[name=aname3]:checked');
+              $dname4 = $('input[name=aname4]:checked');
+              $dname5 = $('input[name=aname5]:checked');
+              if($dname3.length > 0)
+              {
+                  $aname31 = $dname3.val();
+              }
+              if($dname4.length > 0)
+              {
+                  $aname41 = $dname4.val();
+              }
+              if($dname5.length > 0)
+              {
+                  $aname51 = $dname5.val();
+              }
+              $total = Number($aname11) + Number($aname21) + Number($aname31) + Number($aname41) + Number($aname51);
+              $total_aname.html($total);
           });
       });
       </script>
--- application/views/student/ins_eval.php
+++ application/views/student/ins_eval.php
@@ -64,7 +64,7 @@
                                     1.	Demonstrates sensitivity to students’ ability to attend and absorb content information.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="aname" name="aname1" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -72,7 +72,7 @@
                                     2.	Integrates sensitively his/her learning objectives with those of the students in a collaborative process.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="aname" name="aname2" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -80,7 +80,7 @@
                                     3.	Makes self available to students beyond official time
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="aname" name="aname3" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -88,7 +88,7 @@
                                     4.	Regularly comes to class on time, well-groomed and well-prepared to complete assigned responsibilities.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="aname" name="aname4" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -96,7 +96,7 @@
                                     5.	Keeps accurate records of students’ performance and prompt submission of the same.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="aname" name="aname5" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -104,7 +104,7 @@
                                     <strong>Total Score</strong>
                                 </td>
                                 <td colspan="5">
-
+                                    <span id="aname"></span>
                                 </td>
                             </tr>
 
@@ -117,7 +117,7 @@
                                     1.	Demonstrates mastery of the subject matter (explain the subject matter without relying solely on the prescribed textbook).
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="bname" name="bname1" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -125,7 +125,7 @@
                                     2.	Draws and share information on the state on the art of theory and practice in his/her discipline.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="bname" name="bname2" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -133,7 +133,7 @@
                                     3.	Integrates subject to practical circumstances and learning intents/purposes of students.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="bname" name="bname3" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -141,7 +141,7 @@
                                     4.	Explains the relevance of present topics to the previous lessons, and relates the subject matter to relevant current issues and/or daily life activities.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="bname" name="bname4" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -149,7 +149,7 @@
                                     5.	Demonstrates up-to-date knowledge and/or awareness on current trends and issues of the subject.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="bname" name="bname5" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -157,7 +157,7 @@
                                     <strong>Total Score</strong>
                                 </td>
                                 <td colspan="5">
-
+                                    <span id="bname"></span>
                                 </td>
                             </tr>
 
@@ -170,7 +170,7 @@
                                     1.	Creates teaching strategies that allow students to practice using concepts they need to understand (interactive discussion).
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="cname" name="cname1" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -178,7 +178,7 @@
                                     2.	Enhances student self-esteem and/or gives due recognition to students’ performance/potentials.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="cname" name="cname2" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -186,7 +186,7 @@
                                     3.	Allows students to create their own course with objectives and realistically defined student-professor rules and make them accountable for their performance
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="cname" name="cname3" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -194,7 +194,7 @@
                                     4.	Allows students to think independently and make their own decisions and holding them accountable for their performance based largely on their success in executing decisions.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="cname" name="cname4" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -202,7 +202,7 @@
                                     5.	Encourages students to learn beyond what is required and help/guide the students how to apply the concepts learned
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="name" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" name="cname5" class="cname" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -210,7 +210,7 @@
                                     <strong>Total Score</strong>
                                 </td>
                                 <td colspan="5">
-
+                                    <span id="cname"></span>
                                 </td>
                             </tr>
 
@@ -223,7 +223,7 @@
                                     1.	Creates opportunities for intensive and/or extensive contribution of students in the class activities (e.g. breaks class into dyads, triads or buzz/task groups).
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="dname" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="dname" name="dname1" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -231,7 +231,7 @@
                                     2.	Assumes roles as facilitator, resource person, coach, inquisitor, integrator, referee in drawing students to contribute to knowledge and understanding of the concepts at hands.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="dname" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="dname" name="dname2" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -239,7 +239,7 @@
                                     3.	Designs and implements learning conditions and experience that promotes healthy exchange and/or confrontations.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="dname" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="dname" name="dname3" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -247,7 +247,7 @@
                                     4.	Structures/re-structures learning and teaching–learning context to enhance attainment of collective learning objectives.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="dname" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="dname" name="dname4" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -255,7 +255,7 @@
                                     5.	Use of Instructional Materials ((audio/video materials: fieldtrips, film showing, computer aided instruction and etc.) to reinforces learning processes.
                                 </td>
                                 <?php for($i = 5; $i >= 1; $i--){ ?>
-                                    <td> <input type="radio" name="dname" value=""><?php echo $i ?></td>
+                                    <td> <input type="radio" class="dname" name="dname5" value="<?php echo $i ?>"><?php echo $i ?></td>
                                 <?php } ?>
                             </tr>
                             <tr>
@@ -263,7 +263,7 @@
                                     <strong>Total Score</strong>
                                 </td>
                                 <td colspan="5">
-
+                                    <span id="dname"></span>
                                 </td>
                             </tr>
                         </table>
