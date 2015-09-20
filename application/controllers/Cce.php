f1516cd41ab470ab93963b50481e6ba61a6efb79
Jayson Martinez <jlmartinez631@gmail.com>
2015-09-20 09:36:18 +0800

    function to rank a instructor

--- application/controllers/Cce.php
+++ application/controllers/Cce.php
@@ -48,7 +48,7 @@ class Cce extends CI_Controller
     }
 
     // calculate total cce points of instructor
-    function cce_instruc($id)
+    function qce_instruc($id)
     {
         $r = $this->db->get_where('attachment', array('instructor' => $id))->result_array();
         $sum = 0;
@@ -92,5 +92,66 @@ class Cce extends CI_Controller
         $e = $this->db->get_where('tbl_evaluation', array('to_evaluate' => $id, 'evaluator' => $id))->row_array();
         $sum = $e['group1'] + $e['group2'] + $e['group3'] + $e['group4'];
         return $sum * PEER_SELF;
+    }
+
+    function rank($id)
+    {
+        $qce            = $this->qce_instruc($id);
+        // just add the supervisor eval
+        $cce            = $this->self_eval($id) + $this->peer_eval($id) + $this->student_eval($id);
+        $based_point    = ($qce > $cce) ? 'cce' : 'qce';
+        $e              = $this->db->get_where('tbl_userreg', array('id' => $id))->row_array();
+        $position       = $e['position'];
+        $pos            = $this->db->get('points_allocation')->result_array();
+
+
+        foreach ($pos as $key)
+        {
+            if($based_point == 'cce')
+            {
+                $c  = $key['cce_points'];
+                $cc = explode('-', $c);
+                // if the cce points is within the range
+                if($cce >= $cc[0] AND $cce <= $cc[1])
+                {
+                    //just promote if the position bracket is greater the current position
+                    if($key['position'] > $position)
+                    {
+                        //increment position then break
+                        $positon++;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                $c  = $key['qce_points'];
+                $cc = explode('-', $c);
+                if(count($cc) == 1)
+                {
+                    if($qce == $cc[0])
+                    {
+                        if($key['position'] > $position)
+                        {
+                            $position++;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if($qce >= $cc[0] AND $qce <= $cc[1])
+                    {
+                        if($key['position'] > $position)
+                        {
+                            $position++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // update the instructor rank if the rank is change
     }
 }
